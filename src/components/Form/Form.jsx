ac674933e64f3764b1c1d71ae3f8aeda0590c376
armanphero <017911sksaifurrahman@gmail.com>
2023-11-17 14:57:49 -0800

    search implemented

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -21,12 +21,6 @@ const Form = () => {
     return (
         <div className='container my-5 d-flex flex-column align-items-center py-3'>
             <h2 className='text-center text-white'>Contact us</h2>
-            {/* <form action="#" className='w-50 d-flex flex-column gap-4'>
-                <input type="text" name="" id="" className='form-control py-3' placeholder='Your Name' />
-                <input type="email" name="" id="" className='form-control py-3' placeholder='Your Email' />
-                <textarea name="" id="" cols="30" rows="5" className='form-control' placeholder='Write Your Message'></textarea>
-                <button className='btn btn-info'>Submit</button>
-            </form> */}
             <form ref={form} onSubmit={sendEmail} className='w-50'>
                 <label className='text-bold'>Name</label>
                 <input type="text" name="from_name" className='form-control py-3 mb-3' placeholder='Your Name' required />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,18 @@ import { useState } from 'react';
 
 const Header = ({handleSearch}) => {
     const [input, setInput] = useState('');
+    const handleKeyUp = (key) => {
+        if(key === 'Enter'){
+            handleSearch(input);
+        }
+    }
     return (
         <div className='bg-primary'>
             <nav className="navbar px-5 container">
                 <div className="container-fluid">
                     <a className="navbar-brand text-white">Movies</a>
                     <div className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={input} onInput={e => setInput(e.target.value)} />
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={input} onInput={e => setInput(e.target.value)} onKeyUp={(e) => handleKeyUp(e.key)} />
                         <button className="btn btn-danger" type="submit" onClick={() => handleSearch(input)}>Search</button>
                     </div>
                 </div>
